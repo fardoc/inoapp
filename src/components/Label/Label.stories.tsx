22511a7f07a6eefd181c3f55766ca9ce497783ee
K A <kfe00726@gmail.com>
2019-01-06 10:06:36 +0900

    npm install -save @types/node @types/react npm install @storybook/addon-knobs --save-dev npm install @types/storybook__addon-actions --save-dev npm install @types/storybook__addon-knobs --save-dev

--- src/components/Label/Label.stories.tsx
+++ src/components/Label/Label.stories.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { withKnobs, text } from "@storybook/addon-knobs";
 import { Label } from "./Label";
 
-storiesOf("Components", module).add("Label", () => <Label>test</Label>);
+const stories = storiesOf("Components", module);
+stories.addDecorator(withKnobs);
+
+stories.add("Label", () => <Label>{text("Label", "test")}</Label>);
